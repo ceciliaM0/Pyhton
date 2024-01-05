9e3e835a7266a117ebce574bb6ea52087d17aac2
Bolotă Teofil <126006784+TeoB2003@users.noreply.github.com>
2024-01-05 15:47:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
-def counter(exercitiu):
-    file=open("C:\\Users\\EBOLOTTI5\\Downloads\\output.txt", mode='r');
-    no_a1 = {'ERROR': 0, 'DEBUG': 0, 'INFO': 0}
-    no_a2 = {'ERROR': 0, 'DEBUG': 0, 'INFO': 0}
-    no_a3 = {'ERROR': 0, 'DEBUG': 0, 'INFO': 0}
-    no_a4 = {'ERROR': 0, 'DEBUG': 0, 'INFO': 0}
-    info={'BackendApp':no_a1,'FrontendApp':no_a2,'API':no_a3, 'SYSTEM':no_a4}
+def read_log_file():
+    file = open("C:\\Users\\EBOLOTTI5\\Downloads\\output.txt", mode='r')
+    info = {'BackendApp': {'ERROR': 0, 'DEBUG': 0, 'INFO': 0},
+            'FrontendApp': {'ERROR': 0, 'DEBUG': 0, 'INFO': 0},
+            'API': {'ERROR': 0, 'DEBUG': 0, 'INFO': 0},
+            'SYSTEM': {'ERROR': 0, 'DEBUG': 0, 'INFO': 0}}
 
-    for a in file.readlines():
-        cuv=a.split()
-        type=cuv[2][1:-1:]
-        app=cuv[4]
-        if type!= 'INFO':
-         info[app][type]+=1
+    for line in file.readlines():
+        words = line.split()
+        log_type = words[2][1:-1:]
+        app = words[4]
+        if log_type != 'INFO':
+            info[app][log_type] += 1
         else:
-            info[app][type] += 0.5
-       # print(f'Pt {app} am {info[app][type]} pt {type}')
+            info[app][log_type] += 0.5
 
+    file.close()
+    return info
 
-    if exercitiu==1:
-        for z, x in info.items():
-            if z!='SYSTEM':
-             print(f'Pentru {z} avem cifrele:')
-             for y in x.items():
-                print(y)
-             else:
-                 pass
-    elif exercitiu==3:
-        for z, x in info.items():
-            for y in x.items():
-                if y[0]=='ERROR':
-                    print(f'Aplicatia {z} a avut {y[1]} erori')
-    elif exercitiu==4:
-        nrm=0
-        for z, x in info.items():
-            for y in x.items():
-                if y[0]=='ERROR' and y[1]>nrm:
-                    nrm=y[1]
-                    app=z
-        print(f'Cele mai multe erori au fost in aplicatia {app},adica {nrm} erori')
-    elif exercitiu==5:
-        nrm=0
-        for z, x in info.items():
-            for y in x.items():
-                if y[0]=='INFO' and y[1]>nrm:
-                    nrm=y[1]
-                    app=z
-        print(f'Cele mai multe rulari cu succes au fost in aplicatia {app},adica {nrm} rulari')
+info = read_log_file()
 
+def ex1():
+    for app, logs in info.items():
+        if app != 'SYSTEM':
+            print(f'Pentru {app} avem cifrele:')
+            for log_type, count in logs.items():
+                print(log_type, count)
+    return info
 
-counter(5)
+def ex3():
+    rez={}
+    for app, logs in info.items():
+        for log_type, count in logs.items():
+            if log_type == 'ERROR':
+                print(f'Aplicatia {app} a avut {count} erori')
+                rez[app]=count
+    return rez
+
+def ex4():
+    max_errors = 0
+    app_with_errors = ''
+    for app, logs in info.items():
+        for log_type, count in logs.items():
+            if log_type == 'ERROR' and count > max_errors:
+                max_errors = count
+                app_with_errors = app
+    print(f'Cele mai multe erori au fost în aplicatia {app_with_errors}, adică {max_errors} erori')
+    tr=(app_with_errors,max_errors)
+    return tr
+
+def ex5():
+    max_info_count = 0
+    app_with_max_info = ''
+    for app, logs in info.items():
+        for log_type, count in logs.items():
+            if log_type == 'INFO' and count > max_info_count:
+                max_info_count = count
+                app_with_max_info = app
+    print(f'Cele mai multe rulări cu succes au fost în aplicatia {app_with_max_info}, adică {max_info_count} rulări cu succes')
+    return (app_with_max_info,max_info_count)
