67f5f5ac199685b433ff5d05b864bd5f53d88e4e
Bolotă Teofil <126006784+TeoB2003@users.noreply.github.com>
2024-01-09 15:15:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import unittest
 import final
 class MyTestCase(unittest.TestCase):
-    file3="C:\\Users\\EBOLOTTI5\\PycharmProjects\\P1\\t3.txt"
+    file3="C:\\Users\\EBOLOTTI5\\Downloads\\test.txt"
     def test_ex1(self):
         info2=final.read_log_file(self.file3)
         a=final.ex1(info2)
-        self.assertEqual(a, {'BackendApp': {'ERROR': 1, 'DEBUG': 3, 'INFO': 1.0}, 'FrontendApp': {'ERROR': 2, 'DEBUG': 5, 'INFO': 2.0}, 'API': {'ERROR': 1, 'DEBUG': 2, 'INFO': 1}, 'SYSTEM': {'ERROR': 1, 'DEBUG': 1, 'INFO': 2} })
+        self.assertEqual(a, {'BackendApp': {'ERROR': 3, 'DEBUG': 0, 'INFO': 1.5}, 'FrontendApp': {'ERROR': 1, 'DEBUG': 1, 'INFO': 2.0}, 'API': {'ERROR': 0, 'DEBUG': 1, 'INFO': 0.5}, 'SYSTEM': {'ERROR': 0, 'DEBUG': 1, 'INFO': 0} })
     def test_ex3(self):
         info=final.read_log_file(self.file3)
         a=final.ex3(info)
-        self.assertEqual(a, {'BackendApp': 1, 'FrontendApp': 2, 'API': 1, 'SYSTEM': 1})
+        self.assertEqual(a, {'BackendApp': 3, 'FrontendApp': 1, 'API': 0, 'SYSTEM': 0})
     def test_ex5(self):
         info=final.read_log_file(self.file3)
         a=final.ex5(info)
@@ -17,6 +17,6 @@ class MyTestCase(unittest.TestCase):
     def test_ex4(self):
         info=final.read_log_file(self.file3)
         a=final.ex4(info)
-        self.assertEqual(a, ('FrontendApp', 2.0))
+        self.assertEqual(a, ('BackendApp', 3))
 if __name__ == '__main__':
     unittest.main()
